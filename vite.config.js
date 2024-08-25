378eaab5be61b39ecd311a17fe13d30cd9bb752c
Vidyanand <8968964132j.star@gamil.com>
2024-08-25 15:17:18 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        mimeTypes: {
-            'application/javascript': ['js', 'mjs'],
-        },
+  plugins: [react()],
+  server: {
+    mimeTypes: {
+      'application/javascript': ['js', 'mjs', 'jsx'],
     },
+  },
 });
-
